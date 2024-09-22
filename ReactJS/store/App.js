ad6f56f3c02fd4693d22ad9117f1a8e0c2bef867
Nguyen Duy Dong <21521956@gm.uit.edu.vn>
2024-09-23 01:23:59 +0700

    Create App.js

---
+++ ReactJS/store/App.js
@@ -0,0 +1,31 @@
+import {actions, useStore} from './store'
+
+function App() {
+  const [state, dispatch] = useStore()
+  const {todos, todoInput} = state
+  
+  const handleAdd = () => {
+    dispatch(actions.addTodo(todoInput))
+  }
+
+  return (
+      <div style={{padding: 20}}>
+        <h1>Context + useReducer</h1>
+        <input
+          value={todoInput}
+          placeholder='Enter todo...'
+          onChange={e => {
+            dispatch(actions.setTodoInput(e.target.value))
+          }}
+          
+          />
+          <button onClick={handleAdd}> Add
+
+          </button>
+          {todos.map((todo, index) => (
+            <li key={index}>{todo}</li>
+          ))}
+      </div>
+  )
+}
+export default App
